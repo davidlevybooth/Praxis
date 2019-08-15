1511495343b6a6fad0af0ff3c71804d718f387f7
Parker Lloyd <parker@bm-z820.mi.microbiology.ubc.ca>
2019-08-15 14:34:22 -0700

    refactor: Minor change

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ read -p "Enter a name for your pipeline: " name
 conda env create --name $name --file envs/environment.yaml
 #conda activate $name
 
-if [ $(grep 'Praxis' ~/.bashrc |wc -l) -eq 0 ]
+if [ $(grep 'Praxis' ~/.bashrc | wc -l) -eq 0 ]
   then
     cat Praxis_function.txt >> ~/.bashrc
     echo "You must start a new terminal session for the Praxis command to work."
