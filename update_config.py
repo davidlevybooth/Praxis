fcce684496a341781ab01da6ea40fe45f4f724b6
Parker Lloyd <parker@bm-z820.mi.microbiology.ubc.ca>
2019-08-19 15:17:40 -0700

    refactor: Updated logic to only rewrite config if flags present

--- update_config.py
+++ update_config.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import argparse
 import psutil
+import copy
 from os import listdir
 from os.path import isfile, join
 
@@ -17,58 +18,62 @@ parser.add_argument("-f", "--genome_file", type = str, help = "The file containi
 
 args = parser.parse_args()
 
+# open and read the Snakemake config
+file = "config.yaml"
+stream = open(file, "r")
+original_data = yaml.load(stream, yaml.FullLoader)
+new_data = copy.deepcopy(original_data)
+
 for arg in vars(args):
-    # print(arg, getattr(args, arg))
     if getattr(args, arg) is not None:
-
-        # open and read the Snakemake config
-        file = "config.yaml"
-        stream = open(file, "r")
-        data = yaml.load(stream, yaml.FullLoader)
-
         if arg is "method":
             val = getattr(args, arg)
             if val in ["featureCounts", "htseq", "salmon"]:
-                data[arg.upper()] = val
+                new_data[arg.upper()] = val
             else:
                 raise Exception("Requested method is not an available option.")
         elif arg is "aligner":
             val = getattr(args, arg)
             if val in ["bt2", "bbmap", "none"]:
-                data[arg.upper()] = val
+                new_data[arg.upper()] = val
             else:
                 raise Exception("Requested aligner is not an available option.")
         elif arg is "trimmer":
             val = getattr(args, arg)
             if val in ["trimmomatic", "bbduk", "untrimmed"]:
-                data[arg.upper()] = val
+                new_data[arg.upper()] = val
             else:
                 raise Exception("Requested trimmer is not an available option.")
         elif arg is "assembler":
             val = getattr(args, arg)
             if val in ["trinity", "megahit"]:
-                data[arg.upper()] = val
+                new_data[arg.upper()] = val
             else:
                 raise Exception("Requested assembler is not an available option.")
         elif arg is "threads":
             val = getattr(args, arg)
             if val <= psutil.cpu_count():
-                data[arg.upper()] = val
+                new_data[arg.upper()] = val
             else:
                 raise Exception("Number of threads requested exceeds number of available cores.")
         elif arg is "max_memory":
             val = getattr(args, arg)
             if val <= psutil.virtual_memory()[1]:
-                data[arg.upper()] = val
+                new_data[arg.upper()] = val
             else:
                 raise Exception("Requested memory exceeds available memory")
         elif arg is "genome_url":
-            data["genome"]["ncbi_url"] = getattr(args, arg)
+            new_data["genome"]["ncbi_url"] = getattr(args, arg)
         elif arg is "genome_file":
             if getattr(args, arg).split(".")[-1] in ["fasta", "fa", "fna"]:
-                data["genome"]["file"] = getattr(args, arg)
+                new_data["genome"]["file"] = getattr(args, arg)
             else:
                 raise Exception("Provided reference file is not in an accepted format (.fasta, .fa, .fna).")
 
-        with open(file, 'w') as yaml_file:
-            yaml_file.write(yaml.dump(data, default_flow_style=False))
+# update config if changes were made
+if original_data != new_data:
+    with open(file, 'w') as yaml_file:
+        yaml_file.write(yaml.dump(new_data, default_flow_style=False))
+
+# close file stream
+stream.close()
